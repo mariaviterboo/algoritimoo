e05229cada13e38af3bf20e7b5c425f4e7a56290
mariaviterboo <maria.viterbo@escola.pr.gov.br>
2025-10-13 08:20:42 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const avance = document.querySelectorAll('.btn-proximo');
+const avanca = document.querySelectorAll('.btn-proximo');
 
-avance.forEach(button => {
+avanca.forEach(button => {
     button.addEventListener('click', function () {
         const atual = document.querSelector('.ativo');
         const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
         atual.classList.remove('ativo');
-        document.getElementById(proximoPaaso).classList.add('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
     })
 })
